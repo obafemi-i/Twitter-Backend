d06d2e7abf7bc932035ba07f9f53f20ebe93bd98
obafemi-i <obaff76@gmail.com>
2023-12-09 09:27:46 +0100

    linting and prettier setup

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 import express from 'express';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const app = express();
+
+const trial = 'lka';
+
+// eslint-disable-next-line no-console
+console.log(trial);
